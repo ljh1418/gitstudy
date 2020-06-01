0f035043dd059f732c8d385b1f7584d26862d0c4
ljh1418 <ljh1418@gmail.com>
2020-06-01 15:25:31 +0900

    helloName()

--- Hello.java
+++ Hello.java
@@ -4,10 +4,16 @@ public class Hello {
 
 	public static void main(String[] args) {
 		hello();
-		
+		helloName("이준호");
+		helloName("캉테");
 	}
 	public static void hello() {
 		System.out.println("Hello~");
+	}
+	
+	
+	public static void helloName(String name) {
+		System.out.println("Hello" + name);
 	}
 
 }
